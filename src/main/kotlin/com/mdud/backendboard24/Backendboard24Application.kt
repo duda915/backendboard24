f39cdf1ec511a6bce8958c4e47d907aca639dadf
MDud <duda915@gmail.com>
2019-01-28 20:50:39 +0100

    init commit

---
+++ src/main/kotlin/com/mdud/backendboard24/Backendboard24Application.kt
@@ -0,0 +1,12 @@
+package com.mdud.backendboard24
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Backendboard24Application
+
+fun main(args: Array<String>) {
+	runApplication<Backendboard24Application>(*args)
+}
+
