30b99cd91b252dfff301ff7d84fe59edefe3804d
MDud <duda915@gmail.com>
2019-01-28 23:37:55 +0100

    mapping test

--- src/test/kotlin/com/mdud/backendboard24/user/BoardUserMappingTest.kt
+++ src/test/kotlin/com/mdud/backendboard24/user/BoardUserMappingTest.kt
@@ -1,5 +1,6 @@
 package com.mdud.backendboard24.user
 
+import com.mdud.backendboard24.user.authority.AuthorityName
 import com.mdud.backendboard24.user.userauthorities.UserAuthoritiesBuilder
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.transaction.annotation.Transactional
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 
 @RunWith(SpringRunner::class)
@@ -21,6 +24,21 @@ class BoardUserMappingTest {
 
     @Test
     fun save_SaveUserWithoutAuthorities_ShouldSaveUserWithoutAuthorities() {
+        var userWithouthAuthorities = BoardUser("test", "test", mutableSetOf())
+        userWithouthAuthorities = boardUserRepository.save(userWithouthAuthorities)
 
+        assertNotNull(userWithouthAuthorities.id)
+    }
+
+    @Test
+    fun save_SaveUserWithAuthorities_ShouldSaveUserWithAuthorites() {
+        val normalUserAuthorities = userAuthoritiesBuilder.buildUserAuthority(AuthorityName.USER)
+        var normalUser = BoardUser("test", "test", mutableSetOf(normalUserAuthorities))
+
+        normalUser = boardUserRepository.save(normalUser)
+
+        val act = normalUser.userAuthorities.stream().allMatch { it.id != null }
+
+        assertTrue(act)
     }
 }
